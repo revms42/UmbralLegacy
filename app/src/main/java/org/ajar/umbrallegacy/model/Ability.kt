2c2bc2443ff43a7dc9c9b5bb926416bfc62e761e
revms <revms42@gmail.com>
2021-02-08 23:35:17 -0800

    Added faction icons for nosferatu and wight. Nos I'm happy with, wight could use some work.

--- app/src/main/java/org/ajar/umbrallegacy/model/Ability.kt
+++ app/src/main/java/org/ajar/umbrallegacy/model/Ability.kt
@@ -133,8 +133,8 @@ enum class FactionAbility(override val abilityName: Int, override val descriptio
                           @DrawableRes icon: Int = -1, @ColorRes override val primaryColor: Int = -1,
                           @ColorRes override val secondaryColor: Int = -1, override var cost: Int = 1) : AbilityDefinition {
     VAMPIRE_ABILITY(R.string.ability_vampire_name, R.string.ability_vampire_description, R.drawable.ic_faction_vampire, R.color.groupStillbloodPrimary, R.color.groupStillbloodSecondary, R.integer.vampire_cost),
-    NOSFERATU_ABILITY(R.string.ability_nosferatu_name, R.string.ability_nosferatu_description, R.drawable.ic_group_stillblood, R.color.groupStillbloodPrimary, R.color.groupStillbloodSecondary, R.integer.nosferatu_cost),
-    WIGHT_ABILITY(R.string.ability_wight_name, R.string.ability_wight_description, R.drawable.ic_group_stillblood, R.color.groupStillbloodPrimary, R.color.groupStillbloodSecondary, R.integer.wight_cost),
+    NOSFERATU_ABILITY(R.string.ability_nosferatu_name, R.string.ability_nosferatu_description, R.drawable.ic_faction_nosferatu, R.color.groupStillbloodPrimary, R.color.groupStillbloodSecondary, R.integer.nosferatu_cost),
+    WIGHT_ABILITY(R.string.ability_wight_name, R.string.ability_wight_description, R.drawable.ic_faction_wight, R.color.groupStillbloodPrimary, R.color.groupStillbloodSecondary, R.integer.wight_cost),
     ZOMBIE_ABILITY(R.string.ability_zombie_name, R.string.ability_zombie_description, R.drawable.ic_group_stillblood, R.color.groupStillbloodPrimary, R.color.groupStillbloodSecondary, R.integer.zombie_cost),
 
     LYCANTHROPE_ABILITY(R.string.ability_lycanthrope_name, R.string.ability_lycanthrope_description, R.drawable.ic_group_cursed, R.color.groupCursedPrimary, R.color.groupCursedSecondary, R.integer.lychanthrope_cost),
